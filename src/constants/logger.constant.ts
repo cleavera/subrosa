f579eca472c496bb3772a0884bb7a1fe6615e82a
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2020-09-17 13:41:46 +0100

    Refactor where injections get setup

--- src/constants/logger.constant.ts
+++ src/constants/logger.constant.ts
@@ -1,10 +1,3 @@
 import { ConsoleLoggerFactory, Logger } from '@cleavera/debug';
 
-import { LOGGER_TOKEN } from '../tokens/logger.token';
-import { INJECTOR } from './injector.constant';
-
-const LOGGER: Logger = ConsoleLoggerFactory();
-
-INJECTOR.setValue(LOGGER_TOKEN, LOGGER);
-
-export { LOGGER };
+export const LOGGER: Logger = ConsoleLoggerFactory();
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { CONFIG } from './constants/config.constant';
 import { INJECTOR } from './constants/injector.constant';
+import { LOGGER } from './constants/logger.constant';
 import { CONFIG_TOKEN } from './tokens/config.token';
+import { LOGGER_TOKEN } from './tokens/logger.token';
 
 INJECTOR.setValue(CONFIG_TOKEN, CONFIG);
+INJECTOR.setValue(LOGGER_TOKEN, LOGGER);
 
-export { LOGGER } from './constants/logger.constant';
 export { Package } from './classes/package';
+export { LOGGER };
 export { CONFIG };
